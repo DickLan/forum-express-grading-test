fc1c2f3292ed411a3376d05094ed2d1a248a0863
DickLan <pokemaster556610@gmail.com>
2023-09-09 15:04:14 +0800

    feat: update seed files

--- seeders/20230909070151-restaurant-seed-file.js
+++ seeders/20230909070151-restaurant-seed-file.js
@@ -1,7 +1,16 @@
 'use strict'
+// categories seed 要在 restaurant seed之前
+// restaurant seed才讀的到ＦＫ
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // check what id in Categories now
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories',
+      // type: 表示要指定查詢的類別 select表示一個select查詢
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
@@ -11,7 +20,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         descriptions: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       })))
   },
 

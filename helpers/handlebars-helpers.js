dc8157baa4a35388dddbecec505277dbd630ed10
DickLan <pokemaster556610@gmail.com>
2023-09-10 17:23:36 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,10 @@
 const dayjs = require('dayjs')
 
+// helpers 匯出後，需要到 app.js掛載到 app.engine
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
